24bca2146117732903ab5f11da64d0453b8c4434
AniketDange23 <Aniketdange2321@gmail.com>
2022-12-18 17:39:32 +0530

    js task incomplete

---
+++ app.js
@@ -0,0 +1,7 @@
+const   questionDiv = document.getElementById(question); 
+const   sub_1 = document.getElementById(sub_1); 
+const   sub_2 = document.getElementById(sub_2); 
+const   sub_3 = document.getElementById(sub_3); 
+const   sub_4 = document.getElementById(sub_4); 
+
+question.style.visibility(hidden);
